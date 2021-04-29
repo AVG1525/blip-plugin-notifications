ad4ffd43294a2bc35f243c1413d1a175e16e4591
xandeq <xandeq@gmail.com>
2021-04-29 12:43:01 -0300

    Alterações

--- src/pages/App/AppComponent.js
+++ src/pages/App/AppComponent.js
@@ -10,19 +10,17 @@ import { PageHeader } from 'components/PageHeader';
 import { BlipTable } from 'components/BlipTable';
 import { CommonProvider } from 'contexts/CommonContext';
 import { PageTemplate } from 'components/PageTemplate';
-import { BlipTabs } from 'blip-toolkit';
 
 const TABLE_SCHEDULES_MODEL = [
-    { label: 'Name', key: 'name' },
-    { label: 'When', key: 'when' },
+    { label: 'Nome', key: 'name' },
+    { label: 'Quando', key: 'when' },
     { label: 'Status', key: 'status' },
     { label: 'Notification Id', key: 'message' }
 ];
 
 const TABLE_MESSAGES_MODEL = [
-    { label: 'Event', key: 'event' },
-    { label: 'From', key: 'from' },
-    { label: 'To', key: 'to' }
+    { label: 'Evento', key: 'event' },
+    { label: 'Fone', key: 'from' }
 ];
 
 const AppComponent = () => {
@@ -30,6 +28,8 @@ const AppComponent = () => {
     const [schedules, setSchedules] = useState([]);
     const [notifications, setNotifications] = useState([]);
     const [notificationsAux, setnotificationsAux] = useState([]);
+    const [hideList, setHideList] = useState(false);
+    //const [isLoading, setLoading] = useState(true);
 
     const fetchApi = async () => {
         setApplication(await getApplication());
@@ -43,7 +43,6 @@ const AppComponent = () => {
 
     useEffect(() => {
         withLoading(async () => {
-            new BlipTabs('tab-nav');
             await fetchApi();
         });
     }, []);
@@ -51,10 +50,15 @@ const AppComponent = () => {
     const title = `Sample Plugin - ${application.shortName}`;
 
     const handleDetalhes = async (messageId) => {
-        console.log(messageId);
         setNotifications(await getNotifications(messageId));
         setnotificationsAux(await getNotifications(messageId));
         localStorage.setItem('messageId', messageId);
+        setHideList(true);
+    };
+
+    const backToList = (event) => {
+        event.preventDefault();
+        setHideList(false);
     };
 
     const handleSelectEvent = async (event) => {
@@ -65,11 +69,11 @@ const AppComponent = () => {
         }
 
         let notificationsSelected = '';
-        if (value == 'all') {
+        if (value === 'all') {
             notificationsSelected = notifications;
         } else {
             notificationsSelected = notifications.filter((arr) => {
-                return arr.event == value;
+                return arr.event === value;
             });
         }
 
@@ -81,90 +85,99 @@ const AppComponent = () => {
             <div id="main" className="App">
                 <PageHeader title={title} />
                 <PageTemplate title={title}>
-                    <div id="tab-nav" className="bp-tabs-container">
-                        <ul className="bp-tab-nav">
-                            <li>
-                                {/* eslint-disable-next-line */}
-                                <a href="#" data-ref="notifications">
-                                    Notificações agendadas
-                                </a>
-                            </li>
-                            <li>
-                                {/* eslint-disable-next-line */}
-                                <a href="#" data-ref="messagesnotifications">
-                                    Notificações de Mensagens
-                                </a>
-                            </li>
-                        </ul>
-                        <div
-                            className="bp-tab-content fl w-100"
-                            data-ref="notifications"
-                        >
-                            <BlipTable
-                                id_key="name"
-                                model={TABLE_SCHEDULES_MODEL}
-                                data={schedules
-                                    .filter((s) => {
-                                        return s.status === 'executed';
-                                    })
-                                    .map((s) => ({
-                                        name: (
-                                            <a
-                                                style={{
-                                                    color: '#3f7de8',
-                                                    fontWeight: 'bold'
-                                                }}
-                                                href="#"
-                                                onClick={() =>
-                                                    handleDetalhes(s.message.id)
-                                                }
-                                            >
-                                                {s.name
-                                                    ? s.name
-                                                    : 'Ver Notificações'}
-                                            </a>
-                                        ),
-                                        when: s.when,
-                                        status: s.status,
-                                        message: s.message.id
-                                    }))}
-                                can_select={true}
-                                body_height="400px"
-                                selected_items={[]}
-                            />
-                        </div>
-                        <div
-                            className="bp-tab-content fl w-100"
-                            data-ref="messagesnotifications"
-                        >
-                            <div>
-                                Esolha o tipo de evento:
-                                <select
-                                    id="select-event"
-                                    onChange={handleSelectEvent}
-                                >
-                                    <option value="all">Todos</option>
-                                    <option value="accepted">accepted</option>
-                                    <option value="Java">received</option>
-                                    <option value="consumed">consumed</option>
-                                    <option value="failed">failed</option>
-                                </select>
+                    {!hideList && (
+                        <div>
+                            <h3 className="text-center">Notificações agendadas</h3>
+                            <div className="row w-100">
+                                <BlipTable
+                                    id_key="name"
+                                    model={TABLE_SCHEDULES_MODEL}
+                                    data={schedules
+                                        .filter((s) => {
+                                            return s.status === 'executed';
+                                        })
+                                        .map((s) => ({
+                                            name: (
+                                                <a
+                                                    style={{
+                                                        color: '#3f7de8',
+                                                        fontWeight: 'bold'
+                                                    }}
+                                                    href="#"
+                                                    onClick={() =>
+                                                        handleDetalhes(
+                                                            s.message.id
+                                                        )
+                                                    }
+                                                >
+                                                    {s.name
+                                                        ? s.name
+                                                        : 'Ver Notificações'}
+                                                </a>
+                                            ),
+                                            when: s.when,
+                                            status: s.status,
+                                            message: s.message.id
+                                        }))}
+                                    can_select={true}
+                                    body_height="400px"
+                                    selected_items={[]}
+                                />
                             </div>
-                            <BlipTable
-                                id_key="id"
-                                model={TABLE_MESSAGES_MODEL}
-                                data={notificationsAux.map((s) => ({
-                                    event: s?.event,
-                                    from: s?.from,
-                                    to: s?.to
-                                }))}
-                                empty_message="Nenhum registro encontrado"
-                                can_select={true}
-                                body_height="400px"
-                                selected_items={[]}
-                            />
                         </div>
-                    </div>
+                    )}
+                    {hideList && (
+                        <div class="row">
+                            <div class="col-12">
+                                <h3 className="text-center">Lista de Notificações</h3>
+                                <div>
+                                    <p>
+                                        <a
+                                            style={{
+                                                color: '#3f7de8',
+                                                fontWeight: 'bold'
+                                            }}
+                                            href="#"
+                                            onClick={backToList}
+                                        >
+                                            Voltar
+                                        </a>
+                                    </p>
+                                    Esolha o tipo de evento:
+                                    <select
+                                        id="select-event"
+                                        onChange={handleSelectEvent}
+                                    >
+                                        <option value="all">Todos</option>
+                                        <option value="accepted">
+                                            accepted
+                                        </option>
+                                        <option value="Java">received</option>
+                                        <option value="consumed">
+                                            consumed
+                                        </option>
+                                        <option value="failed">failed</option>
+                                    </select>
+                                </div>
+                                <div id="divNotifications" class="row">
+                                    <div class="col-12">
+                                        <BlipTable
+                                            id_key="id"
+                                            model={TABLE_MESSAGES_MODEL}
+                                            data={notificationsAux.map((s) => ({
+                                                event: s?.event,
+                                                from: s?.from.split('/')[1].split('%')[0]
+                                            }))}
+                                            empty_message="Nenhum registro encontrado"
+                                            can_select={true}
+                                            body_height="400px"
+                                            selected_items={[]}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </PageTemplate>
             </div>
         </CommonProvider>
